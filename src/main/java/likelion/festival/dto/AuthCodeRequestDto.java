2c03931a6f865f2fe34cd14134e46940331951df
noturavrigk <ksm92038386@gmail.com>
2025-05-13 10:19:28 +0900

    Fix: change login flow

--- src/main/java/likelion/festival/dto/AuthCodeRequestDto.java
+++ src/main/java/likelion/festival/dto/AuthCodeRequestDto.java
@@ -1,13 +1,12 @@
 package likelion.festival.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class AuthCodeRequestDto {
+    @NotNull(message = "인증 코드 값이 없습니다")
     private String code;
-    private String error;
-    private String error_description;
-    private String state;
 }
