bc1fdeceb9b9a761aba371d40577562fd2255e00
mongsam2 <songjaehyun121476@gmail.com>
2025-05-17 17:18:47 +0900

    fix: 관리자가 주점 웨이팅 조회 시, pubId전달 없이 인증 토큰에서 주점 정보 추출해서 사용

--- src/main/java/likelion/festival/service/AdminService.java
+++ src/main/java/likelion/festival/service/AdminService.java
@@ -4,14 +4,19 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
+import likelion.festival.config.CustomUserDetails;
+import likelion.festival.domain.Pub;
 import likelion.festival.domain.User;
 import likelion.festival.domain.Waiting;
 import likelion.festival.dto.AdminDeleteDto;
 import likelion.festival.dto.AdminWaitingList;
 import likelion.festival.dto.FcmTokenRequest;
+import likelion.festival.exceptions.AdminPermissionException;
 import likelion.festival.exceptions.EntityNotFoundException;
 import likelion.festival.repository.WaitingRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,11 +32,23 @@ public class AdminService {
     private final GuestWaitingService guestWaitingService;
     private final FCMService fcmService;
     private final WaitingRepository waitingRepository;
+    private final PubService pubService;
 
-    public List<AdminWaitingList> getWaitingList(Long pubId) {
+    public List<AdminWaitingList> getWaitingList() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        boolean isAdmin = userDetails.getAuthorities().stream()
+                .anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN"));
+
+        if (!isAdmin) {
+            throw new AdminPermissionException("관리자 권한이 없습니다. 관리자 계정으로 로그인해주세요.");
+        }
+        String name = userDetails.getUsername();
+        Pub pub = pubService.getPubByName(name);
+
         List<AdminWaitingList> adminWaitingList = new ArrayList<>();
-        adminWaitingList.addAll(waitingService.getAdminWaitingList(pubId));
-        adminWaitingList.addAll(guestWaitingService.getAdminWaitingList(pubId));
+        adminWaitingList.addAll(waitingService.getAdminWaitingList(pub.getId()));
+        adminWaitingList.addAll(guestWaitingService.getAdminWaitingList(pub.getId()));
 
         adminWaitingList.sort(Comparator.comparing(AdminWaitingList::getWaitingNum));
         return adminWaitingList;
