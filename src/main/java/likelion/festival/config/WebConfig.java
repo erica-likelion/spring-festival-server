4bf81423f0ac5f987ffc42f7e423d76e6f732937
mongsam2 <songjaehyun121476@gmail.com>
2025-05-19 22:50:35 +0900

    feat: 관리자 앱 허용

--- src/main/java/likelion/festival/config/WebConfig.java
+++ src/main/java/likelion/festival/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .exposedHeaders("Authorization") // 프론트에서 읽을 수 있게
+                //.exposedHeaders("Authorization") // 프론트에서 읽을 수 있게
                 .allowCredentials(true); // 쿠키 허용
     }
 }
