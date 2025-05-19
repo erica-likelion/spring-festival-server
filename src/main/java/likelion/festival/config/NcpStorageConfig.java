db99716869f8354842728cac90d9d5864cb569f6
mongsam2 <songjaehyun121476@gmail.com>
2025-05-19 15:01:08 +0900

    feat: NcpStorageConfig 클래스 작성

---
+++ src/main/java/likelion/festival/config/NcpStorageConfig.java
@@ -0,0 +1,38 @@
+package likelion.festival.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+
+import java.net.URI;
+
+@Configuration
+@RequiredArgsConstructor
+public class NcpStorageConfig {
+
+    private final NcpStorageProperties properties;
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(
+                properties.getAccessKey(),
+                properties.getSecretKey()
+        );
+        return S3Client.builder()
+                .endpointOverride(URI.create(properties.getEndpoint()))
+                .region(Region.of(properties.getEndpoint()))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .serviceConfiguration(
+                    S3Configuration.builder()
+                        .pathStyleAccessEnabled(true)  // ✅ 필수 설정!
+                        .build()
+                )
+                .build();
+    }
+}
