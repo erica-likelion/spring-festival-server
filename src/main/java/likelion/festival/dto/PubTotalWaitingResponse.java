57cdf9168f92b1796267391ad95e5103d7facf97
mongsam2 <songjaehyun121476@gmail.com>
2025-05-21 09:31:16 +0900

    feat: 응답 형식을 정의한 dto 작성

---
+++ src/main/java/likelion/festival/dto/PubTotalWaitingResponse.java
@@ -0,0 +1,10 @@
+package likelion.festival.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PubTotalWaitingResponse {
+    private Integer waitingCount;
+}
