78a45758f8c205dc127af4251fd26660697dd44f
mongsam2 <songjaehyun121476@gmail.com>
2025-07-22 18:13:38 +0900

    chore: 주석 추가

--- src/main/java/likelion/festival/service/LostItemService.java
+++ src/main/java/likelion/festival/service/LostItemService.java
@@ -29,6 +29,8 @@ public class LostItemService {
 
     /*
     * 모든 분실물 정보를 리스트 조회
+    * output:
+    *   - "LostItemResponseDto"를 담은 리스트
     */
     public List<LostItemResponseDto> allLostItem() {
         List<LostItem> lostItems = lostItemRepository.findAll();
@@ -40,6 +42,8 @@ public class LostItemService {
     * 특정 분실 날짜에 해당하는 분실물들만 따로 리스트 조회
     * input:
     *   - lostDate: ["1일차", "2일차", "3일차"] 중 하나에 해당
+    * output:
+    *   - "LostItemResponseDto"를 담은 리스트
     */
     public List<LostItemResponseDto> findByLostDate(String lostDate) {
         List<LostItem> lostItems = lostItemRepository.findByFoundDate(lostDate);
@@ -51,6 +55,8 @@ public class LostItemService {
     * 분실물(LostItem) 중에  "name"속성이 검색어를 포함하고 있는 경우만 필터링해서 조회
     * input:
     *   - name: 검색하고자 하는 분실물 이름
+    * output:
+    *   - "LostItemResponseDto"를 담은 리스트
     */
     public List<LostItemResponseDto> findByName(String name) {
         List<LostItem> lostItems = lostItemRepository.findByNameContaining(name);
@@ -63,6 +69,8 @@ public class LostItemService {
     * input:
     *   - lostDate: ["1일차", "2일차", "3일차"] 중 하나에 해당
     *   - name: 검색하고자 하는 분실물 이름
+    * return:
+    *   - "LostItemResponseDto"를 담은 리스트
     */
     public List<LostItemResponseDto> findByLostDateAndName(String lostDate, String name) {
         List<LostItem> lostItems = lostItemRepository.findByFoundDateAndNameContaining(lostDate, name);
@@ -74,8 +82,8 @@ public class LostItemService {
     * input:
     *   - dto: "image" 필드가 비어있는 LostItemRequestDto
     *   - image: 사용자가 보낸 분실물 이미지 파일
-    * logic:
-    *   - 이미지 파일을 cloud storage에 업로드 후, dto에 url을 담아서 전달
+    * return:
+    *   - DB에 저장된 분실물 객체 반환
     */
     public LostItem addLostItem(LostItemRequestDto dto, MultipartFile image) {
         String imageKey;
