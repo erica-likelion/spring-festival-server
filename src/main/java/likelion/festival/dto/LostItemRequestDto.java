e779461e970ffb46790c7514ddca35aa5fd6b00b
mongsam2 <songjaehyun121476@gmail.com>
2025-07-23 14:43:57 +0900

    chore: LostItem Dto 주석 작성

--- src/main/java/likelion/festival/dto/LostItemRequestDto.java
+++ src/main/java/likelion/festival/dto/LostItemRequestDto.java
@@ -16,25 +16,26 @@ import org.springframework.web.multipart.MultipartFile;
 public class LostItemRequestDto {
 
     // ** 파일 형식을 받지 않고, 파일이 저장된 url을 문자열로 받아와야 합니다.
-    private String image;
+    private String image; // 분실물 이미지 파일의 url
+
 
     @NotBlank(message = "name은 필수입니다.")
-    private String name;
+    private String name; // 분실물 등록 글 제목
 
     @NotBlank(message = "description은 필수입니다.")
-    private String description;
+    private String description; // 분실물 설명
 
     @NotNull(message = "staffNotified은 필수입니다.")
-    private Boolean staffNotified;
+    private Boolean staffNotified; // 분실물을 스태프에게 전달했는지 여부
 
     @NotBlank(message = "foundLocation은 필수입니다.")
-    private String foundLocation;
+    private String foundLocation; // 분실물을 찾은 장소
 
     @NotBlank(message = "foundDate은 필수입니다.")
-    private String foundDate;
+    private String foundDate; // 분실물을 찾은 날짜 (1일차, 2일차, 3일차)
 
     @NotBlank(message = "foundTime은 필수입니다.")
-    private String foundTime;
+    private String foundTime; // 분실물을 찾은 시간 (ex. 13:00~14:00)
 
     public void setImage(String image) {
         this.image = image;
--- src/main/java/likelion/festival/dto/LostItemResponseDto.java
+++ src/main/java/likelion/festival/dto/LostItemResponseDto.java
@@ -7,15 +7,18 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class LostItemResponseDto {
-    private Long id;
-    private String image;
-    private String name;
-    private String description;
-    private Boolean staffNotified;
-    private String foundLocation;
-    private String foundDate;
-    private String foundTime;
+    private Long id; // pk
+    private String image; // 분실물 이미지 파일의 url
+    private String name; // 분실물 등록 글 제목
+    private String description; // 분실물 설명
+    private Boolean staffNotified; // 분실물을 스태프에게 전달했는지 여부
+    private String foundLocation; // 분실물을 찾은 장소
+    private String foundDate; // 분실물을 찾은 날짜 (1일차, 2일차, 3일차)
+    private String foundTime; // 분실물을 찾은 시간 (ex. 13:00~14:00)
 
+    /*
+    * LostItem 객체 -> dto 변환
+    * */
     public LostItemResponseDto(LostItem lostItem) {
         this.id = lostItem.getId();
         this.image = lostItem.getImage();
